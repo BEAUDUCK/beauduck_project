3ff409d95944d4884d4bc7900a18911f7928660d
ęšėė (KIM A YOUNG) <joen00@naver.com>
2023-01-16 23:05:07 +0900

    [add] AI machine learning

---
+++ 김아영/firstproject/manage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+import numpy as np
+import dlib
+import cv2
+
+RIGHT_EYE = list(range(36, 42))
+LEFT_EYE = list(range(42, 48))
+MOUTH = list(range(48, 68))
+NOSE = list(range(27, 36))
+EYEBROWS = list(range(17, 27))
+JAWLINE = list(range(1, 17))
+ALL = list(range(0, 68))
+EYES = list(range(36, 48))
+
+def main():
+    predictor_file = 'shape_predictor_68_face_landmarks.dat' #-- 자신의 개발 환경에 맞게 변경할 것
+    image_file = './songkang.png' #-- 자신의 개발 환경에 맞게 변경할 것
+
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(predictor_file)
+
+    image = cv2.imread(image_file)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    rects = detector(gray, 1)
+    print("Number of faces detected: {}".format(len(rects)))
+
+
+    for (i, rect) in enumerate(rects):
+        points = np.matrix([[p.x, p.y] for p in predictor(gray, rect).parts()])
+        show_parts = points[ALL]
+        for (i, point) in enumerate(show_parts):
+            x = point[0,0]
+            y = point[0,1]
+            cv2.circle(image, (x, y), 1, (0, 255, 255), -1)
+            cv2.putText(image, "{}".format(i + 1), (x, y - 2),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
+
+    cv2.imshow("Face Landmark", image)
+    cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    main()
