e63a3512e37870096ad2ce5c2f778abf551b8e1c
unknown <zhujack@gmail.com>
2023-01-16 23:53:56 +0900

    redux_practice

---
+++ FE/주승/redux_practice/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// 여기서 부터 추가 
+import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// rootReducer 를 가진 Store 생성
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
