e8a919111040a2bfcecb64439b173b9ca183f1b8
choi1087 <adcdm87@gmail.com>
2023-01-18 11:13:04 +0900

    test port 8080

---
+++ 최규림/test8080/src/main/java/com/example/test8080/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.test8080.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "TEST START 8080";
+    }
+}
