8b0a885c251c1072610b9dbc7b647a155620118f
choi1087 <adcdm87@gmail.com>
2023-01-18 11:13:04 +0900

    test port 8080

---
+++ 최규림/test8080/src/test/java/com/example/test8080/Test8080ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test8080;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test8080ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
