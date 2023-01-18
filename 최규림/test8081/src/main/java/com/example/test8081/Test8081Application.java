ac141c96a4937dcaaac4a99a509159bdbe1e7985
choi1087 <adcdm87@gmail.com>
2023-01-18 11:15:47 +0900

    test port 8081

---
+++ 최규림/test8081/src/main/java/com/example/test8081/Test8081Application.java
@@ -0,0 +1,13 @@
+package com.example.test8081;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test8081Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test8081Application.class, args);
+    }
+
+}
---
+++ 최규림/test8081/src/test/java/com/example/test8081/Test8081ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test8081;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test8081ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
