fd4ca8fad440295a591587ba3dc12008baf24046
yonghyunc <chyh9218@gmail.com>
2023-01-20 01:24:39 +0900

    [Feat]: Make ConsultingPage Skeleton

--- beauduck-front/src/pages/ConsultingPage.jsx
+++ beauduck-front/src/pages/ConsultingPage.jsx
@@ -1,11 +1,39 @@
 import Banner from "../components/banner/Banner";
+import ConsultingList from "../features/help/ConsultingList";
 
 const ConsultingPage = () => {
+  const TestList = [
+    {
+      id: 1,
+      title: "하하하",
+      text: "하이하이하이하이하이",
+      host: "나님",
+      status: "진행 중", 
+    },
+    {
+      id: 2,
+      title: "키키키",
+      text: "안녕안녕안녕안녕",
+      host: "나야나",
+      status: "모집 중", 
+    },
+    {
+      id: 3,
+      title: "룰루",
+      text: "어서오세요 빨리오세요 제발오세요",
+      host: "룰루",
+      status: "모집 중", 
+    }
+  ]
+
   return (
-    <div>
+    <>
       <Banner bannerStyle={"help-ban"}/>
-      <h1>도와덕 메인</h1>
-    </div>
+      <div className="container">
+        <h2>진행 중인 컨설팅</h2>
+        <ConsultingList consultingList={TestList}/>
+      </div>
+    </>
   );
 };
 
--- beauduck-front/src/store.js
+++ beauduck-front/src/store.js
@@ -1,4 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import consultingReducer from './features/help/ConsultingSlice';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    consulting: consultingReducer,
+  },
 });
