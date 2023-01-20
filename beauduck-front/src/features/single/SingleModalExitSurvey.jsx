075030b4650b5b463d4478ab9334b5aaec510687
Yonghyunc <chyh9218@gmail.com>
2023-01-20 22:00:27 +0900

    [Feat]: Make SinglePage Skeleton

---
+++ beauduck-front/src/features/single/SingleModalExitSurvey.jsx
@@ -0,0 +1,126 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useRef, useState } from 'react';
+import Button from '../../components/button/Button';
+import RatingButton from '../../components/button/RatingButton';
+
+const SingleModalExitSurvey = () => {
+  const [score, setScore] = useState(0);
+  const [star1, setStar1] = useState(false);
+  const [star2, setStar2] = useState(false);
+  const [star3, setStar3] = useState(false);
+  const [star4, setStar4] = useState(false);
+  const [star5, setStar5] = useState(false);
+
+  // const [stars, setStars] = useState({
+  //   star1: false,
+  //   star2: false,
+  //   star3: false,
+  //   star4: false,
+  //   star5: false,
+  // });
+
+  // 아무것도 선택 안 했을 때 선택 유도
+  const starRef = useRef();
+
+  const changeStar1 = () => {
+    setStar1(true);
+    setStar2(false);
+    setStar3(false);
+    setStar4(false);
+    setStar5(false);
+  };
+  const changeStar2 = () => {
+    setStar1(true);
+    setStar2(true);
+    setStar3(false);
+    setStar4(false);
+    setStar5(false);
+  };
+  const changeStar3 = () => {
+    setStar1(true);
+    setStar2(true);
+    setStar3(true);
+    setStar4(false);
+    setStar5(false);
+  };
+  const changeStar4 = () => {
+    setStar1(true);
+    setStar2(true);
+    setStar3(true);
+    setStar4(true);
+    setStar5(false);
+  };
+  const changeStar5 = () => {
+    setStar1(true);
+    setStar2(true);
+    setStar3(true);
+    setStar4(true);
+    setStar5(true);
+  };
+
+  useEffect(() => {
+    if (star5) {
+      setScore(5);
+    } else if (star4) {
+      setScore(4);
+    } else if (star3) {
+      setScore(3);
+    } else if (star2) {
+      setScore(2);
+    } else if (star1) {
+      setScore(1);
+    }
+    console.log(score);
+  }, [star1, star2, star3, star4, star5, score]);
+
+  return (
+    <div className="survey-modal">
+      <h2>평가해덕</h2>
+      <hr />
+      <div ref={starRef}>
+        <span onClick={changeStar1}>
+          {!star1 ? (
+            <FontAwesomeIcon className="star-icon" icon="fa-regular fa-star" />
+          ) : (
+            <FontAwesomeIcon className="star-icon" icon="fa-solid fa-star" />
+          )}
+        </span>
+        <span onClick={changeStar2}>
+          {!star2 ? (
+            <FontAwesomeIcon className="star-icon" icon="fa-regular fa-star" />
+          ) : (
+            <FontAwesomeIcon className="star-icon" icon="fa-solid fa-star" />
+          )}
+        </span>
+        <span onClick={changeStar3}>
+          {!star3 ? (
+            <FontAwesomeIcon className="star-icon" icon="fa-regular fa-star" />
+          ) : (
+            <FontAwesomeIcon className="star-icon" icon="fa-solid fa-star" />
+          )}
+        </span>
+        <span onClick={changeStar4}>
+          {!star4 ? (
+            <FontAwesomeIcon className="star-icon" icon="fa-regular fa-star" />
+          ) : (
+            <FontAwesomeIcon className="star-icon" icon="fa-solid fa-star" />
+          )}
+        </span>
+        <span onClick={changeStar5}>
+          {!star5 ? (
+            <FontAwesomeIcon className="star-icon" icon="fa-regular fa-star" />
+          ) : (
+            <FontAwesomeIcon className="star-icon" icon="fa-solid fa-star" />
+          )}
+        </span>
+      </div>
+      <br />
+      <div>
+        <RatingButton text={'재밌는 과정'} />
+      </div>
+      <Button text={'완료'} />
+    </div>
+  );
+};
+
+export default SingleModalExitSurvey;
