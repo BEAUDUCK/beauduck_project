981438138b8386822b9a0f593414716d1ff67114
최규림 <adcdm87@gmail.com>
2023-01-24 22:01:22 +0900

    [docs] delete folders

--- back/beauduck-auth/src/main/java/com/ssafy/beauduckauth/controller/TestController.java
+++ back/beauduck-auth/src/main/java/com/ssafy/beauduckauth/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.test8080.controller;
+package com.ssafy.beauduckauth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,8 +6,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String test(){
-        return "TEST START 8080";
-    }
 }
