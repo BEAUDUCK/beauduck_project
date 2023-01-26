5beee3918044ad120ad3908aef37bacc14869b71
백자민 <gbjhgy126@gmail.com>
2023-01-26 16:21:26 +0900

    [feat]: Modify Entity class

--- back/beauduck-board/src/main/java/com/ssafy/beauduckboard/dto/info/BoardInfoDto.java
+++ back/beauduck-board/src/main/java/com/ssafy/beauduckboard/dto/info/BoardInfoDto.java
@@ -1,4 +1,6 @@
 package com.ssafy.beauduckboard.dto.info;
 
 public class BoardInfoDto {
+
+
 }
--- back/beauduck-board/src/main/java/com/ssafy/beauduckboard/entity/BoardInfoEntity.java
+++ back/beauduck-board/src/main/java/com/ssafy/beauduckboard/entity/BoardInfoEntity.java
@@ -1,4 +1,24 @@
 package com.ssafy.beauduckboard.entity;
 
-public class BoardInfoEntity {
-}
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardInfoEntity{
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int board_id;
+        //    @JsonIgnore => member response안 받아옴
+        private String board_title;
+        private String board_writer;
+        private String board_content;
+        private int board_count;
+
+    }
