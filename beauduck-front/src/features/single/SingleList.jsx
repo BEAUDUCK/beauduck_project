ee1ef158119e647feb8c0b9cb6cf75b10bc24026
Yonghyunc <chyh9218@gmail.com>
2023-01-26 17:41:34 +0900

    [Feat]: Create SingleModalInfo select btn

--- beauduck-front/src/features/single/SingleList.jsx
+++ beauduck-front/src/features/single/SingleList.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from 'react';
+import Paging from '../../components/pagination/Paging';
 import './Single.style.scss';
 import SingleListItem from './SingleListItem';
 
 const SingleList = ({ modeList }) => {
+  const [currentRooms, setCurrentRooms] = useState([]); // 보여줄 게시글
+  const [page, setPage] = useState(1); // 현재 페이지
+  const handlePageChange = (page) => {
+    setPage(page);
+  };
+  const [postPerPage] = useState(12);
+
+  const indefOfLastRoom = page * postPerPage;
+  const indefOfFirstRoom = indefOfLastRoom - postPerPage;
+
+  useEffect(() => {
+    setCurrentRooms(modeList.slice(indefOfFirstRoom, indefOfLastRoom));
+  }, [indefOfFirstRoom, indefOfLastRoom, page]);
+
   return (
-    <div className="modeList">
-      {modeList.map((item) => (
-        <SingleListItem key={item.id} modeItem={item} />
-      ))}
-    </div>
+    <>
+      <div className="modeList">
+        {currentRooms.map((item) => (
+          <SingleListItem key={item.id} modeItem={item} />
+        ))}
+      </div>
+      <Paging
+        totalCount={modeList.length}
+        page={page}
+        postPerPage={postPerPage}
+        pageRangeDisplayed={5}
+        handlePageChange={handlePageChange}
+      />
+    </>
   );
 };
 
--- beauduck-front/src/features/single/SingleModalInfo.jsx
+++ beauduck-front/src/features/single/SingleModalInfo.jsx
@@ -1,8 +1,101 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMakeupDetail } from './SingleSlice';
+import { useState } from 'react';
+import Button from '../../components/button/Button';
 
 // 메이크업 상세 조회 API
-const SingleModalInfo = () => {
-  return <div></div>;
+const SingleModalInfo = ({ makeupId }) => {
+  const dispatch = useDispatch();
+  // const { makeup } = useSelector((state) => state.makeupDetail);
+  useEffect(() => {
+    dispatch(getMakeupDetail(makeupId));
+  }, [dispatch]);
+
+  // 지금은 테스트 데이터 넣어놓음
+  const makeup = {
+    id: 1,
+    title: '데일리 메이크업',
+    content: '일상생활에서 부담없이 할 수 있는 데일리 메이크업입니다.',
+    duration: 30,
+    score: 4.5,
+    count: 120,
+    img: 'https://i.pinimg.com/236x/00/23/13/0023139711735d03774be660adcad98c.jpg',
+  };
+
+  let category = [];
+  const [isSelected1, setIsSelected1] = useState(false);
+  const [isSelected2, setIsSelected2] = useState(false);
+  const [isSelected3, setIsSelected3] = useState(false);
+  const [isSelected4, setIsSelected4] = useState(false);
+  const [isSelected5, setIsSelected5] = useState(false);
+
+  const nums = [1, 2, 3, 4, 5];
+
+  const selectSubmit = () => {
+    nums.forEach((num) => {
+      if (eval(`isSelected${num}`)) {
+        // 어떤 값으로 넣을지는 백과 논의해야 함
+        category.push(num);
+      }
+    });
+    console.log(category);
+  };
+  return (
+    <div className="makeup-detail">
+      <div className="makeup-detail-top">
+        <div className="makeup-detail-left">
+          <h3>{makeup.title}</h3>
+          <div className="star-info">
+            <FontAwesomeIcon className="star-icon" icon="fa-solid fa-star" />
+            <p>{makeup.score}</p>
+            <p>({makeup.count})</p>
+          </div>
+          <p className="makeup-content">{makeup.content}</p>
+          <p className="makeup-content">소요시간 : {makeup.duration} 분</p>
+        </div>
+        <div className="makeup-detail-right">
+          <img src={makeup.img} alt="" />
+        </div>
+      </div>
+      <div className="select-course">
+        <button
+          onClick={() => setIsSelected1(!isSelected1)}
+          className={isSelected1 && 'selected'}
+        >
+          피부
+        </button>
+        <button
+          onClick={() => setIsSelected2(!isSelected2)}
+          className={isSelected2 && 'selected'}
+        >
+          눈썹
+        </button>
+        <button
+          onClick={() => setIsSelected3(!isSelected3)}
+          className={isSelected3 && 'selected'}
+        >
+          눈
+        </button>
+        <button
+          onClick={() => setIsSelected4(!isSelected4)}
+          className={isSelected4 && 'selected'}
+        >
+          윤곽
+        </button>
+        <button
+          onClick={() => setIsSelected5(!isSelected5)}
+          className={isSelected5 && 'selected'}
+        >
+          립
+        </button>
+      </div>
+      <p className="describe-text">*진행하려는 과정을 선택하세요.</p>
+      <Button text={'시작하기'} onClickEvent={selectSubmit} />
+    </div>
+  );
 };
 
 export default SingleModalInfo;
