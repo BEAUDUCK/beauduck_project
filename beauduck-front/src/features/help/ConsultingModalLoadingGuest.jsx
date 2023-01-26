14fb22415e17606a47dda63dc633a17948fc94ee
Yonghyunc <chyh9218@gmail.com>
2023-01-26 13:40:16 +0900

    [Feat]: Change modal style and thunck function style

--- beauduck-front/src/features/help/ConsultingModalLoadingGuest.jsx
+++ beauduck-front/src/features/help/ConsultingModalLoadingGuest.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 import { useNavigate } from '../../../node_modules/react-router-dom/dist/index';
 
-const ConsultingModalLoadingGuest = ({host}) => {
+const ConsultingModalLoadingGuest = ({ host }) => {
   const navigate = useNavigate();
 
   const [isReady, setIsReady] = useState(false);
@@ -13,36 +13,37 @@ const ConsultingModalLoadingGuest = ({host}) => {
   };
 
   const goBack = () => {
-    navigate('/help',  { replace: true});
-    // 근데 모달이면 이미 이 페이지 내부인데..?
     // 방에 들어와있는 유저에서 제외하고, 모달을 닫는 로직 작성
-  }
+  };
 
   return (
-    <div className={["loading-modal", "guest"].join(" ")}>
-      <FontAwesomeIcon 
-        className="back-icon" 
-        icon="fa-solid fa-circle-chevron-left" 
+    <div className={['loading-modal', 'guest'].join(' ')}>
+      <FontAwesomeIcon
+        className="back-icon"
+        icon="fa-solid fa-circle-chevron-left"
         onClick={goBack}
       />
       <h3>{host}님이 도움을 요청 중이덕</h3>
-      <button 
+      <button
         onClick={ChangeStatus}
-        className={["loading-bigbtn", "guest-bigbtn"].join(" ")}
-      >READY</button>
-      {isReady 
-        ? <FontAwesomeIcon 
-            className={["battery", "full"].join(" ")} 
-            icon="fa-solid fa-battery-full" 
-          /> 
-        : <FontAwesomeIcon 
-            className={["battery", "less"].join(" ")} 
-            icon="fa-solid fa-battery-quarter" 
-          />
-      }
+        className={['loading-bigbtn', 'guest-bigbtn'].join(' ')}
+      >
+        READY
+      </button>
+      {isReady ? (
+        <FontAwesomeIcon
+          className={['battery', 'full'].join(' ')}
+          icon="fa-solid fa-battery-full"
+        />
+      ) : (
+        <FontAwesomeIcon
+          className={['battery', 'less'].join(' ')}
+          icon="fa-solid fa-battery-quarter"
+        />
+      )}
       <p>중도 퇴장 시에는 패널티가 부과됩니다.</p>
     </div>
-  )
+  );
 };
 
 export default ConsultingModalLoadingGuest;
