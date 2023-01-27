a9e88a14c87dfe7b1d83401ab688fea822b5999c
김아영 <joen00@naver.com>
2023-01-27 09:05:25 +0900

    [feat] : Add boardQaResponseDto, boardQaRequestDto, boardQaController, boardQaRepository, boardQaService

---
+++ back/beauduck-board/src/main/java/com/ssafy/beauduckboard/dto/qa/BoardQaRequestDto.java
@@ -0,0 +1,39 @@
+package com.ssafy.beauduckboard.dto.qa;
+
+import com.ssafy.beauduckboard.entity.BoardQaEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiParam;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@NoArgsConstructor
+@ApiModel(value = "BoardQaRequestDto : 질문 게시판 요청 정보", description = "질문 게시판 요청 정보를 나타낸다.")
+public class BoardQaRequestDto {
+
+    @ApiParam(value = "질문 게시판 member_id", required = true)
+    private String member_id;
+    @ApiParam(value = "질문 게시판 제목", required = true)
+    private String title;
+    @ApiParam(value = "질문 게시판 작성자", required = true)
+    private String writer;
+    @ApiParam(value = "질문 게시판 내용", required = true)
+    private String content;
+    @ApiParam(value = "질문 게시판 삭제 여부", required = true)
+    private Boolean isActive;
+
+    @Builder
+    public BoardQaEntity ToEntity() {
+        return BoardQaEntity.builder()
+                .member_id(member_id)
+                .title(title)
+                .writer(writer)
+                .content(content)
+                .isActive(isActive)
+                .build();
+    }
+
+}
---
+++ back/beauduck-board/src/main/java/com/ssafy/beauduckboard/repository/BoardQaRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.beauduckboard.repository;
+
+import com.ssafy.beauduckboard.entity.BoardQaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardQaRepository extends JpaRepository<BoardQaEntity, Integer> {
+}
