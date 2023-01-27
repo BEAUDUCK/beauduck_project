a8f84445ddda2a2eedc97352e2046657ff5b223a
김아영 (KIM A YOUNG) <joen00@naver.com>
2023-01-27 15:22:01 +0900

    [feat] : Add BoardQaGetList methods Controller

--- back/beauduck-board/src/main/java/com/ssafy/beauduckboard/controller/BoardQaController.java
+++ back/beauduck-board/src/main/java/com/ssafy/beauduckboard/controller/BoardQaController.java
@@ -7,27 +7,31 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Api("질문 게시판 컨트롤러 API")
-@RequestMapping(path="/board/qa")
+//@RequestMapping(path="/board/qa")
 public class BoardQaController {
 
     private final BoardQaService boardQaService;
 
     @ApiOperation(value = "질문 게시판 작성", notes = "질문 게시판 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
-    @PostMapping
-    public ResponseEntity<String> savePost(@RequestBody BoardQaRequestDto request) {
+    @PostMapping("/board/qa")
+    public ResponseEntity<String> createBoard(@RequestBody BoardQaRequestDto request) {
         boolean result = boardQaService.createBoard(request);
         if (result)
             return new ResponseEntity<String>("success", HttpStatus.ACCEPTED);
         else
             return new ResponseEntity<String>("fail", HttpStatus.BAD_REQUEST);
     }
+
+    @ApiOperation(value = "질문 게시판 리스트 보기", notes = "질문 게시판 정보를 리스트 보기한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+    @GetMapping("/board/qa")
+    public ResponseEntity<?> getBoardList(){
+        return new ResponseEntity(boardQaService.readBoardList(), HttpStatus.ACCEPTED);
+    }
+
 
 }
