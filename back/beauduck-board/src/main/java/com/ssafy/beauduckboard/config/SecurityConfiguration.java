3ed9ef3249b913dbbe2853b29eaa56b5227c9751
백자민 <gbjhgy126@gmail.com>
2023-01-27 09:44:32 +0900

    [build]: Add SecurityConfiguration

---
+++ back/beauduck-board/src/main/java/com/ssafy/beauduckboard/config/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.ssafy.beauduckboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests().antMatchers("/").permitAll().and().build();
+    }
+}
