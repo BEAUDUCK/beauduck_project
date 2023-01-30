a34f1a56e01479c1078bfdef6936006e82603f49
Yonghyunc <chyh9218@gmail.com>
2023-01-30 11:28:34 +0900

    [Feat]: Add Together Page UI

--- beauduck-front/src/pages/TogetherPage.jsx
+++ beauduck-front/src/pages/TogetherPage.jsx
@@ -1,8 +1,45 @@
+import { useState } from 'react';
+import '../features/together/Together.style.scss';
+import Banner from '../components/banner/Banner';
+import TogetherList from '../features/together/TogetherList';
+import TogetherModalCreate from '../features/together/TogetherModalCreate';
+
 const TogetherPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const isOpenModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const testList = [
+    {
+      id: 1,
+      title: '투게덕덕',
+      text: '안녕안녕안녕',
+      host: '장현혁',
+    },
+    {
+      id: 2,
+      title: '오세요',
+      text: '하이하이하이',
+      host: 'Jack',
+    },
+    {
+      id: 3,
+      title: '하위',
+      text: '안녕안녕',
+      host: 'ZoD',
+    },
+  ];
   return (
-    <div>
-      <h1>투게덕 메인</h1>
-    </div>
+    <>
+      <Banner bannerStyle={'together-ban'} />
+      <div className="container">
+        <h1>투게덕</h1>
+        <button onClick={isOpenModal}>방 만들기</button>
+        {isOpen && <TogetherModalCreate isOpenModal={isOpenModal} />}
+        <TogetherList roomList={testList} />
+      </div>
+    </>
   );
 };
 
