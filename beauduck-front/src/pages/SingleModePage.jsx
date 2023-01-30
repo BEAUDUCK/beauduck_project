e5d9c37df6bd21a58be1ad6a407894ae17afad78
Yonghyunc <chyh9218@gmail.com>
2023-01-30 13:16:54 +0900

    [Feat]: Add facemesh in SingleModePage

--- beauduck-front/src/pages/SingleModePage.jsx
+++ beauduck-front/src/pages/SingleModePage.jsx
@@ -1,125 +1,15 @@
-import { FaceMesh } from '@mediapipe/face_mesh';
-import * as Facemesh from '@mediapipe/face_mesh';
-import * as cam from '@mediapipe/camera_utils';
-import Webcam from 'react-webcam';
-import { useRef, useEffect } from 'react';
+// import { FaceMesh } from '@mediapipe/face_mesh';
+import FacemeshFeature from '../features/facemesh/FacemeshFeature';
+// import * as Facemesh from '@mediapipe/face_mesh';
+// import * as cam from '@mediapipe/camera_utils';
+// import Webcam from 'react-webcam';
+// import { useRef, useEffect } from 'react';
 
 const SingleModePage = () => {
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
-  let camera;
-  const connect = window.drawConnectors;
-
-  const onResults = (results) => {
-    // console.log(results);
-    canvasRef.current.width = webcamRef.current.video.videoWidth;
-    canvasRef.current.height = webcamRef.current.videoheight;
-
-    const canvasElement = canvasRef.current;
-    const canvasCtx = canvasElement.getContext('2d');
-    canvasCtx.save();
-
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(
-      results.image,
-      0,
-      0,
-      canvasElement.width,
-      canvasElement.height,
-    );
-
-    if (results.multiFaceLandmarks) {
-      for (const landmarks of results.multiFaceLandmarks) {
-        connect(canvasCtx, landmarks, Facemesh.FACEMESH_TESSELATION, {
-          color: '#C0C0C070',
-          lineWidth: 1,
-        });
-        connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYE, {
-          color: 'FF3030',
-        });
-        connect(canvasCtx, landmarks, Facemesh.FACEMESH_RIGHT_EYEBROW, {
-          color: 'FF3030',
-        });
-        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYE, {
-          color: 'FF3030',
-        });
-        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LEFT_EYEBROW, {
-          color: 'FF3030',
-        });
-        connect(canvasCtx, landmarks, Facemesh.FACEMESH_FACE_OVAL, {
-          color: 'FF3030',
-        });
-        connect(canvasCtx, landmarks, Facemesh.FACEMESH_LIPS, {
-          color: 'FF3030',
-        });
-      }
-    }
-  };
-
-  useEffect(() => {
-    const faceMesh = new FaceMesh({
-      locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
-      },
-    });
-    // faceMesh.initialize();
-    faceMesh.setOptions({
-      maxNumFaces: 1,
-      minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5,
-    });
-
-    faceMesh.onResults(onResults);
-
-    // create Web Camera
-    if (
-      typeof webcamRef.current !== 'undefined' &&
-      webcamRef.current !== null
-    ) {
-      camera = new cam.Camera(webcamRef.current.video, {
-        onFrame: async () => {
-          await faceMesh.send({ image: webcamRef.current.video });
-        },
-        width: 640,
-        height: 480,
-      });
-      camera.start();
-    }
-  }, []);
-
   return (
-    <div className="container">
-      <h1>따라해덕 하는 중~</h1>
-      <Webcam
-        ref={webcamRef}
-        style={{
-          position: 'absolute',
-          marginRight: 'auto',
-          marginLeft: 'auto',
-          left: 0,
-          right: 0,
-          textAlign: 'center',
-          zIndex: 9,
-          width: 640,
-          height: 480,
-        }}
-      />
-      <canvas
-        ref={canvasRef}
-        style={{
-          position: 'absolute',
-          marginRight: 'auto',
-          marginLeft: 'auto',
-          left: 0,
-          right: 0,
-          textAlign: 'center',
-          zIndex: 9,
-          width: 640,
-          height: 480,
-        }}
-      />
-    </div>
+    <>
+      <FacemeshFeature />
+    </>
   );
 };
-
 export default SingleModePage;
