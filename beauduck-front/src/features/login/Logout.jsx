8a47abf1b0751d49138939b02e13f1aab5d64b8b
ë°ėŖŧėš <juseungpark@naver.com>
2023-02-01 17:55:42 +0900

    [feat]:social api logic and cookie

---
+++ beauduck-front/src/features/login/Logout.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { useCookies } from 'react-cookie'
+import { useState, useEffect } from 'react'
+import { useNavigate  } from 'react-router-dom';
+
+
+
+    
+  };
+  return (
+    <div className='container'>
+       <button>Logout</button>
+    </div>
+  )
+}
+
+export default Logout;
