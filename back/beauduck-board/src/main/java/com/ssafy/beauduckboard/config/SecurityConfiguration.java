6892b24af68bf310d148eee6368166c4fb2fc523
최규림 <adcdm87@gmail.com>
2023-02-01 22:04:25 +0900

    [feat]: Configure Cors

--- back/beauduck-board/src/main/java/com/ssafy/beauduckboard/config/SecurityConfiguration.java
+++ back/beauduck-board/src/main/java/com/ssafy/beauduckboard/config/SecurityConfiguration.java
@@ -6,10 +6,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -17,4 +19,11 @@ public class SecurityConfiguration {
     }
 
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE");
+    }
 }
