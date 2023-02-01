ae31e738b4cfe71559cc735beaa58b3451f469a2
choi1087 <adcdm87@gmail.com>
2023-02-01 17:52:27 +0900

    [feat]: Change responseDto related login

---
+++ back/beauduck-auth/src/main/java/com/ssafy/beauduckauth/dto/auth/LoginResponseDto.java
@@ -0,0 +1,18 @@
+package com.ssafy.beauduckauth.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginResponseDto {
+    private String memberId;
+    private String name;
+    private String email;
+    private String sex;
+    private String phoneNumber;
+}
