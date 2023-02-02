f1b7276436e720bd0267476a309300425ceb43dd
박주승 <juseungpark@naver.com>
2023-02-02 15:54:11 +0900

    [fix]:error editing

--- beauduck-front/src/features/login/LogoutModal.jsx
+++ beauduck-front/src/features/login/LogoutModal.jsx
@@ -1,36 +1,35 @@
-// import axios from 'axios';
-// import { useCookies } from 'react-cookie'
-// import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
+import { useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom';
 
+const LogoutModal = () => {
+  const [getCookie, setCookie, removeCookie] = useCookies(['cookie_name']);
+  const navigate = useNavigate();
 
-// const LogoutModal = () => {
-//   const navigate = useNavigate();
-//   const [cookies, setCookie, removeCookie] = useCookies(['cookie_name']);
-//   const Logout = async (accessToken) => {
-//     axios
-//       .get(`http://i8b306.p.ssafy.io:8080/naver/logout?accessToken=${accessToken}`)
-//       .then((res) => {
-//       console.log(res.data.status);
-//       removeCookie('accessToken');
-//       localStorage.removeItem('refreshToken');
-//       })
-//       return(
-//         navigate('/'))
-      
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   };
+  const [accessToken, setAccessToken] = useState(getCookie('accessToken'));
 
+  const Logout = async () => {
+    try {
+      const res = await axios.get(`http://i8b306.p.ssafy.io:8080/naver/logout?accessToken=${accessToken}`);
+      console.log('시작');
+      console.log(res.data.status);
+      removeCookie('accessToken');
+      localStorage.removeItem('refreshToken');
+      navigate('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-//   return (
-//     <div className='logout-container'>
-//       <span>
-//         정말 로그 아웃하시겠습니까?
-//       </span>
-//       <button onClick={Logout}> 네 </button>
-//     </div>
-//   )
-// };
+  return (
+    <div className='logout-container'>
+      <span>
+        정말 로그 아웃하시겠습니까?
+      </span>
+      <button onClick={Logout}> 네 </button>
+    </div>
+  );
+};
 
-// export default LogoutModal;
+export default LogoutModal;
