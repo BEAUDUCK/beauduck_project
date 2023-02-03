afe69d7affcce56406933acd4ecd864b3a257c19
Hyokiz <janghh97@gmail.com>
2023-02-03 23:32:41 +0900

    [Style]: style help page

--- beauduck-front/src/features/help/ConsultingList.jsx
+++ beauduck-front/src/features/help/ConsultingList.jsx
@@ -4,7 +4,7 @@ import ConsultingListItem from '../help/ConsultingListItem';
 // 클릭하면 참가하기 등장하자
 const ConsultingList = ({ consultingList }) => {
   return (
-    <div>
+    <div className='consulting-list'>
       {consultingList.map((item) => (
         <ConsultingListItem key={item.id} consultingItem={item} />
       ))}
