3df46fc4d4edf0a8f6dd8b6d09d5f3d02272b259
Yonghyunc <chyh9218@gmail.com>
2023-02-05 15:58:05 +0900

    [Feat]: Erase error code

--- beauduck-front/src/features/ranking/RankingList.jsx
+++ beauduck-front/src/features/ranking/RankingList.jsx
@@ -1,30 +1,21 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import NavigationIcon from '@mui/icons-material/Navigation';
-
 const RankingList = ({ dummyData }) => {
-
   const rankingData = dummyData.slice(0, 10).sort(function (a, b) {
-    return b.exp - a.exp
-  }
-  )
-  
+    return b.exp - a.exp;
+  });
+
   return (
     <div className="RankingList">
       {rankingData.map((it, index) => {
         if (index < 3) {
           return (
-            <div className="ranking-top" >
+            <div className="ranking-top">
               <div>{index + 1}</div>
               <div>{it.badge}</div>
               <div>{it.nickname}</div>
               <div>{it.exp}</div>
             </div>
-          )
+          );
         } else {
           return (
             <div className="ranking-others">
@@ -33,11 +24,11 @@ const RankingList = ({ dummyData }) => {
               <div>{it.nickname}</div>
               <div>{it.exp}</div>
             </div>
-          )
-        } 
+          );
+        }
       })}
     </div>
-  )
+  );
 };
 
 export default RankingList;
