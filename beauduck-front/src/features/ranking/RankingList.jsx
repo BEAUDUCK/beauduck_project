af8d6fa28a7a0b54665880d7190bf18db9359398
Hyokiz <janghh97@gmail.com>
2023-02-05 17:01:51 +0900

    [Refactor]: refactoring code

--- beauduck-front/src/features/ranking/RankingList.jsx
+++ beauduck-front/src/features/ranking/RankingList.jsx
@@ -1,10 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import NavigationIcon from '@mui/icons-material/Navigation';
 
 const RankingList = ({ dummyData }) => {
 
