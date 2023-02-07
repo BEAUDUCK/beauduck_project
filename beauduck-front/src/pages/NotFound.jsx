6cab88991133dfe12d318c4ae1365cb158dad743
Yonghyunc <chyh9218@gmail.com>
2023-02-08 02:16:44 +0900

    [Feat]: Block header routing

---
+++ beauduck-front/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import duck from '../assets/duck3.jpg';
+
+const NotFound = () => {
+  return (
+    <div className="container">
+      <h1>응 돌아가~</h1>
+      <img src={duck} alt="" />
+    </div>
+  );
+};
+
+export default NotFound;
