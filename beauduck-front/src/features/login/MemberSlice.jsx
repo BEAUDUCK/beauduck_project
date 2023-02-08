86e4fa607b39b4eeaf0b48a396828780e7f8c0f3
Yonghyunc <chyh9218@gmail.com>
2023-02-08 17:37:56 +0900

    [Feat]: Split Login & Signup logic

--- beauduck-front/src/features/login/MemberSlice.jsx
+++ beauduck-front/src/features/login/MemberSlice.jsx
@@ -1,19 +1,32 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { async } from 'q';
 import { useCookies } from 'react-cookie';
 
 const server = 'http://3.38.169.2:8080/';
 const global = 'http://i8b306.p.ssafy.io:8080/';
+
 export const UserLogin = createAsyncThunk(
   'member/login',
   async (accessToken) => {
-    console.log('로그인할게!', accessToken);
+    console.log('로그인');
     const res = await axios.get(
       `${server}naver/login?accessToken=${accessToken}`,
     );
     return res.data;
   },
 );
+
+export const signUp = createAsyncThunk('member/signup', async (payload) => {
+  console.log('회원가입');
+  const res = await axios
+    .post(`${server}naver/signup`, payload)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.log(err));
+  return res.data;
+});
 
 export const checkToken = createAsyncThunk('member/checkToken', async () => {
   const [cookies, setCookie, removeCookie] = useCookies(['cookie_name']);
@@ -29,6 +42,9 @@ export const memberSlice = createSlice({
     memberId: '',
     name: '',
     nickName: '',
+    // 회원가입
+    isSignup: false,
+    memberInfo: [],
   },
   reducers: {
     getMemberId: (state, action) => {
@@ -43,14 +59,20 @@ export const memberSlice = createSlice({
       state.name = '';
       state.nickName = '';
     },
+    goToLogin: (state, action) => {
+      state.isSignup = !state.isSignup;
+      state.memberInfo = action.payload;
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(UserLogin.fulfilled, (state, action) => {
       state.memberId = action.payload.data.memberId;
       state.name = action.payload.data.name;
+      state.nickName = action.payload.data.nickName;
     });
   },
 });
 
-export const { getMemberId, getNickName, removeMember } = memberSlice.actions;
+export const { getMemberId, getNickName, removeMember, goToLogin } =
+  memberSlice.actions;
 export default memberSlice.reducer;
--- beauduck-front/src/pages/SignupPage.jsx
+++ beauduck-front/src/pages/SignupPage.jsx
@@ -1,27 +1,27 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import { useState, useRef } from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
-import { getAccessToken } from '../api/cookie';
-import { UserLogin, getNickName } from '../features/login/MemberSlice';
 import { useDispatch } from 'react-redux';
+import './Signup.style.scss';
+import { goToLogin, signUp } from '../features/login/MemberSlice';
 
 const SignupPage = () => {
   const dispatch = useDispatch();
+
   const [content, setContent] = useState('');
   const [nickName, setNickName] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const accessToken = getAccessToken();
+
+  const REDIRECT_URI = 'http://localhost:3000/Api/Naver';
+  const CLIENT_ID = 'V5gN96q3kFtGfUK7PUds';
+  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&state=STATE_STRING&redirect_uri=${REDIRECT_URI}`;
 
   const [imgFile, setImgFile] = useState('');
   const imgRef = useRef();
+  const nicknameRef = useRef();
+  const contentRef = useRef();
+
+  const [isUsable, setIsUsable] = useState(false);
 
   const saveImgFile = () => {
     const file = imgRef.current.files[0];
@@ -31,143 +31,105 @@ const SignupPage = () => {
       setImgFile(reader.result);
     };
   };
-  const theme = createTheme();
-
-  // 닉네임 길이 + 중복 확인
-  const handleSubmit = async (e) => {
-    setNickName(e.target.value);
-
-    e.preventDefault();
-
-    if (nickName.length > 1) {
-      NickNameCheck(nickName);
-    } else {
-      setErrorMessage('닉네임의 길이가 너무 짧습니다.');
-    }
-  };
 
   // 닉네임 중복 확인
   const NickNameCheck = async (nickName) => {
     axios
       .get(`http://3.38.169.2:8080/members/check/${nickName}`)
       .then((res) => {
-        console.log(res.data.data)
         if (!res.data.data) {
           setErrorMessage('사용 가능한 닉네임입니다');
+          setIsUsable(true);
         } else {
           setErrorMessage('중복된 닉네임입니다.');
+          setIsUsable(false);
         }
       });
   };
 
+  const checkNickname = () => {
+    if (nickName.length > 1) {
+      NickNameCheck(nickName);
+    } else {
+      setErrorMessage('닉네임의 길이가 너무 짧습니다.');
+      setIsUsable(false);
+    }
+  };
+
   // 회원가입
-  const Signup = async (e) => {
+  const submitSignup = async (e) => {
     e.preventDefault();
 
-    console.log('회원가입');
-    const data = {
-      accessToken,
-      nickName,
-      content,
-      img: '',
-    };
+    if (!nickName || !isUsable) {
+      nicknameRef.current.focus();
+      return;
+    }
 
-    axios
-      .post('http://3.38.169.2:8080/naver/signup', data)
-      .then((res) => {
-        console.log('res', res);
-        dispatch(getNickName(nickName));
-        dispatch(UserLogin(accessToken));
-        alert('회원가입 완료');
-        window.location.href = '/';
-      })
-      .catch((error) => {
-        console.log(error);
-        alert('가입할 수 없습니다.');
-      });
+    const payload = [nickName, content, imgFile];
+    console.log(payload);
+    dispatch(goToLogin(payload));
+    window.location.replace(NAVER_AUTH_URL);
   };
 
   return (
-    <div>
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 10,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Typography component="h1" variant="h5">
-              회원가입
-            </Typography>
-            <Box component="form" noValidate sx={{ mt: 3 }}>
-              <img
-                className="imgbox"
-                src={imgFile ? imgFile : '/images/default.png'}
-                alt="사진"
-              ></img>
-              <input
-                className="input-box"
-                type="file"
-                accept="image/*"
-                onChange={saveImgFile}
-                ref={imgRef}
-              ></input>
-
-              <br></br>
-              <br></br>
-              <br></br>
-
-              <Grid container spacing={3}>
-                <Grid item xs={12} sm={12}>
-                  <TextField
-                    required
-                    id="outlined-required"
-                    label="닉네임(필수)"
-                    onChange={handleSubmit}
-                    // value={nickName}
-                    fullWidth
-                    defaultValue=""
-                  />
-                </Grid>
-                <p>{errorMessage ? errorMessage : ' '}</p>
-                <Grid item xs={12} sm={12}>
-                  <TextField
-                    id="outlined-multiline-static"
-                    label="자기소개를 입력해주세요"
-                    multiline
-                    fullWidth
-                    rows={4}
-                    // defaultValue=""
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                  />
-                </Grid>
-
-                <Grid item xs={12}>
-                  <p>
-                    완료하기 버튼을 클릭하면 BeauDuck의 약관, 개인정보처리방침
-                    및 쿠키 정책에 동의하게 됩니다.
-                  </p>
-                </Grid>
-              </Grid>
-              <Button
-                onClick={Signup}
-                type="submit"
-                fullWidth
-                variant="outlined"
-                color="success"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                완료하기
-              </Button>
-            </Box>
-          </Box>
-        </Container>
-      </ThemeProvider>
+    <div className="signup-div">
+      <div className="signup-div-left">
+        {/* <img
+          src="https://i.pinimg.com/564x/db/78/9b/db789bc183a83a2791ce05cccffb332d.jpg"
+          alt=""
+        /> */}
+      </div>
+      <div className="signup-div-right">
+        <h1>회원가입</h1>
+        <form onSubmit={submitSignup} className="signup-form">
+          <img
+            className="imgbox"
+            src={imgFile ? imgFile : '/images/default.png'}
+            alt="사진"
+          ></img>
+          <label htmlFor="imgFile" className="label-img">
+            프로필 사진 업로드
+          </label>
+          <input
+            className="input-box"
+            id="imgFile"
+            type="file"
+            accept="image/*"
+            onChange={saveImgFile}
+            ref={imgRef}
+          ></input>
+          <label htmlFor="nickname" className="label-common">
+            닉네임*
+          </label>
+          <span onClick={checkNickname}>중복 확인</span>
+          <input
+            ref={nicknameRef}
+            id="nickname"
+            type="text"
+            placeholder="닉네임을 입력해주세요"
+            value={nickName}
+            onChange={(e) => setNickName(e.target.value)}
+          />
+          {errorMessage ? (
+            <p className="errormessage">{errorMessage}</p>
+          ) : (
+            <></>
+          )}
+          <label htmlFor="content" className="label-common">
+            자기소개
+          </label>
+          <textarea
+            ref={contentRef}
+            id="content"
+            cols="20"
+            rows="5"
+            placeholder="자기소개를 입력해주세요"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
+          <button>완료</button>
+        </form>
+      </div>
     </div>
   );
 };
