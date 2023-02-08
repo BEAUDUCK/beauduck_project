fabe1c6ac6ab6e5cfb1623085d4af3f7c1ed6e2a
Yonghyunc <chyh9218@gmail.com>
2023-02-08 17:37:56 +0900

    [Feat]: Split Login & Signup logic

--- beauduck-front/src/features/login/NaverLogin.jsx
+++ beauduck-front/src/features/login/NaverLogin.jsx
@@ -3,19 +3,19 @@ import { useCookies } from 'react-cookie';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMemberId, UserLogin } from './MemberSlice';
+import { goToLogin, signUp, signup, UserLogin } from './MemberSlice';
 
 const NaverLogin = () => {
   const dispatch = useDispatch();
   const code = new URL(window.location.href).searchParams.get('code');
   const state = new URL(window.location.href).searchParams.get('state');
   const [cookies, setCookie, removeCookie] = useCookies(['cookie_name']);
-
   const navigate = useNavigate();
-  const nickName = useSelector((state) => state.member);
-  console.log(code, state);
+  const { isSignup, memberInfo } = useSelector((state) => state.member);
+
   // 토큰 발급
   const getToken = async () => {
+    console.log('토큰 발급');
     axios
       .get(`http://3.38.169.2:8080/naver/callback?code=${code}&state=${state}`)
       .then((res) => {
@@ -31,17 +31,38 @@ const NaverLogin = () => {
             expires: expireDate,
           },
         );
-        alert('인증 완료');
-        console.log('토큰 발급함~');
-        dispatch(UserLogin(accessToken));
-        console.log('로그인해');
-        navigate('/');
+        if (!isSignup) {
+          console.log('로그인만 할거야야야ㅑ');
+          dispatch(UserLogin(accessToken)).then(() => {
+            navigate(-1);
+          });
+        } else {
+          console.log('회원가입부터 할거야야야야ㅑ');
+          const data = {
+            accessToken,
+            nickName: memberInfo[0],
+            content: memberInfo[1],
+            img: memberInfo[2],
+          };
+          console.log(data);
+          dispatch(goToLogin([]));
+          dispatch(signUp(data))
+            .then((response) => {
+              console.log('히얼', response);
+              dispatch(UserLogin(accessToken)).then(() => {
+                navigate('/');
+              });
+            })
+            .catch((err) => {
+              console.log('여기', err);
+            });
+          // navigate('/signup');
+        }
       })
       .catch((error) => {
         console.log(error);
       });
   };
-
 
   useEffect(() => {
     getToken();
