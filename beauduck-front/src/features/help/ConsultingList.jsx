25c62d0801d5dbda348e2d4b439b5ceb1dacbcff
Yonghyunc <chyh9218@gmail.com>
2023-02-08 22:39:17 +0900

    [Feat]: Add API request

--- beauduck-front/src/features/help/ConsultingList.jsx
+++ beauduck-front/src/features/help/ConsultingList.jsx
@@ -4,8 +4,8 @@ import ConsultingListItem from '../help/ConsultingListItem';
 // 클릭하면 참가하기 등장하자
 const ConsultingList = ({ consultingList }) => {
   return (
-    <div className='consulting-list'>
-      <div className='consulting-list-child'>
+    <div className="consulting-list">
+      <div className="consulting-list-child">
         {consultingList.map((item) => (
           <ConsultingListItem key={item.id} consultingItem={item} />
         ))}
