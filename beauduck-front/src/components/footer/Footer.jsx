169f723ce9ffae45b4cc051e8150cbacbae7fadb
박주승 <juseungpark@naver.com>
2023-02-09 12:57:57 +0900

    checkout ~1 style-front/UI

--- beauduck-front/src/components/footer/Footer.jsx
+++ beauduck-front/src/components/footer/Footer.jsx
@@ -3,7 +3,14 @@ import './Footer.style.scss';
 const Footer = () => {
   return (
     <div className="footer">
-      <p>여긴 Footer 다!</p>
+      <nav>
+        <a href="https://edu.ssafy.com/" target='_blank'>Company</a>
+      </nav>
+      <p>
+        <span>creater : 애인이덕</span><br />
+        <span>email : </span><br />
+        <span>Copyright 2023. </span>
+      </p>
     </div>
   );
 };
--- beauduck-front/src/features/board/BoardListItem.jsx
+++ beauduck-front/src/features/board/BoardListItem.jsx
@@ -1,23 +1,34 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
+import BoardInfoPage from '../../pages/BoardInfoPage';
+import BoardQnAPage from '../../pages/BoardQnAPage';
 
 const BoardListItem = ({ boardItem, idx, isInfo }) => {
   const navigate = useNavigate();
-
-  const goToWhere = () => {
-    if (isInfo) {
-      navigate(`/board/info/${boardItem.id}`);
-    } else {
-      navigate(`/board/qna/${boardItem.id}`);
-    }
-  };
+  const [isCheck, setCheck] = useState(false);
+  // const goToWhere = () => {
+  //   if (isInfo) {
+  //     navigate(`/board/info/${boardItem.id}`);
+  //   } else {
+  //     navigate(`/board/qna/${boardItem.id}`);
+  //   }
+  // };
   return (
-    <div className="board-list" onClick={goToWhere}>
+    <>
+    <div className="board-list" onClick={() => {setCheck((e) => !e);}}>
       <p>{idx + 1}</p>
       <p className="board-title">{boardItem.title}</p>
       <p>{boardItem.writer}</p>
       <p>{boardItem.count}</p>
       <p>{boardItem.likes}</p>
     </div>
+    
+    {isCheck && (
+      <div>{isInfo ? (
+        <BoardInfoPage />
+        ): (<BoardQnAPage/>)}</div>
+    ) } 
+    </>
   );
 };
 
