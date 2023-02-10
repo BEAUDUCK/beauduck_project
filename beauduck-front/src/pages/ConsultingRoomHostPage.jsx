234ab4caba9a7e0dcba7fe6553b9373fc4e9ee0c
Hyokiz <janghh97@gmail.com>
2023-02-10 09:55:27 +0900

    [Fix]: fixed openvidu

--- beauduck-front/src/pages/ConsultingRoomHostPage.jsx
+++ beauduck-front/src/pages/ConsultingRoomHostPage.jsx
@@ -1,5 +1,9 @@
 import CustomRoomComponent from "../openvidu/components/CustomRoomComponent";
 
+// 컨설팅 받는 페이지
+// roomId로 data dispatch
+// 후에 useSelector 로 hostNickname, userCount, userList 받아오기
+
 const ConsultingRoomPage = () => {
   return (
     <div className="full-screen">
