700b0c7950654093d699cfebe9ffbe417e2b4cd3
최규림 <adcdm87@gmail.com>
2023-02-11 02:42:18 +0900

    [feat]: Add ssl

--- back/beauduck-makeup/src/main/java/com/ssafy/beauduckmakeup/BeauduckMakeupApplication.java
+++ back/beauduck-makeup/src/main/java/com/ssafy/beauduckmakeup/BeauduckMakeupApplication.java
@@ -17,39 +17,5 @@ public class BeauduckMakeupApplication {
         SpringApplication.run(BeauduckMakeupApplication.class, args);
     }
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        // Enable SSL Trafic
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-
-        // Add HTTP to HTTPS redirect
-        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-
-        return tomcat;
-    }
-
-    /*
-    We need to redirect from HTTP to HTTPS. Without SSL, this application used
-    port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
-    redirected to HTTPS on 8443.
-     */
-    private Connector httpToHttpsRedirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(8082);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
-    }
 
 }
