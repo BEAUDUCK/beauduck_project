ccd679a4bf65be67c39ebf359d3c2a6b010cdf6c
박주승 <juseungpark@naver.com>
2023-02-10 14:37:51 +0900

    [fix]:header and footer UI

--- beauduck-front/src/components/footer/Footer.jsx
+++ beauduck-front/src/components/footer/Footer.jsx
@@ -3,7 +3,14 @@ import './Footer.style.scss';
 const Footer = () => {
   return (
     <div className="footer">
-      <p>여긴 Footer 다!</p>
+      <div></div>
+      <p>
+        <div> <a href="https://edu.ssafy.com/">Company</a> </div>
+        <div>creater : 애인이덕</div>
+        <div>email : ssafy@ssafy.com</div>
+        <div>Copyright 2023. 애인이덕. All Rights Reserved.</div>
+      </p>
+      <div></div>
     </div>
   );
 };
--- beauduck-front/src/components/header/Header.jsx
+++ beauduck-front/src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import './Header.style.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../../assets/logo_original.png';
+import toggle from '../../assets/hamburger.png'
 import { useState } from 'react';
 import LoginModal from '../../features/login/LoginModal';
 import LogoutModal from '../../features/login/LogoutModal';
@@ -13,56 +14,45 @@ const Header = () => {
 
   const token = localStorage.getItem('refreshToken');
   const { memberId } = useSelector((state) => state.member);
-
-  const [isOpen, setIsOpen] = useState(false);
-  const isOpenModal = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const [isOpen2, setIsOpen2] = useState(false);
-  const isOpenModal2 = () => {
-    setIsOpen2(!isOpen2);
+  
+  const [menuToggle, setMenuToggle] = useState(false);
+  const menuOnOff = () => {
+    setMenuToggle(menuToggle => !menuToggle);
   };
 
   return (
-    <div className="header">
-      <div className="header-content">
-        <div onClick={() => navigate('/')}>
-          <img className="header-logo" src={logo} alt="logo" />
-          <h3 className="logo-name">뷰덕</h3>
-        </div>
-        <div className="header-nav">
-          {/* <p onClick={() => navigate('/help')}>도와덕</p>
-          <p onClick={() => navigate('/single')}>따라해덕</p>
-          <p onClick={() => navigate('/together')}>투게덕</p>
-          <p onClick={() => navigate('/board')}>쑥덕쑥덕</p>
-          <p onClick={() => navigate('/rank')}>랭킹</p> */}
-          <p onClick={() => navigate('/no')}>도와덕</p>
-          <p onClick={() => navigate('/no')}>따라해덕</p>
-          <p onClick={() => navigate('/no')}>투게덕</p>
-          <p onClick={() => navigate('/no')}>쑥덕쑥덕</p>
-          <p onClick={() => navigate('/no')}>랭킹</p>
-        </div>
-        <div className="header-auth">
-          <p onClick={() => navigate('/profile')}>
-            <FontAwesomeIcon className="user-icon" icon="fa-regular fa-user" />
-          </p>
-          {/* <p onClick={isOpenModal}> 
-          LOGIN
-          {isOpen && <LoginModal isOpenModal={isOpenModal} />}
-          </p> */}
-          {!memberId ? (
-            <p>
-              <LoginModal /> / <p onClick={() => navigate('/signup')}>SIGNUP</p>
-            </p>
-          ) : (
-            <p>
-              <LogoutModal />
-            </p>
-          )}
-        </div>
+    <nav className="header">
+      <div class="header-name"
+      onClick={() => navigate('/')}>
+        <img className="header-logo" src={logo} alt="logo" />
+        <h3>뷰덕</h3>
       </div>
-    </div>
+      <ul className="header-menu">
+        {/* <p onClick={() => navigate('/help')}>도와덕</p>
+        <p onClick={() => navigate('/single')}>따라해덕</p>
+        <p onClick={() => navigate('/together')}>투게덕</p>
+        <p onClick={() => navigate('/board')}>쑥덕쑥덕</p>
+        <p onClick={() => navigate('/rank')}>랭킹</p> */}
+        <li onClick={() => navigate('/no')}>도와덕</li>
+        <li onClick={() => navigate('/no')}>따라해덕</li>
+        <li onClick={() => navigate('/no')}>투게덕</li>
+        <li onClick={() => navigate('/no')}>쑥덕쑥덕</li>
+      
+
+      </ul>
+      <ul className="header-auth">
+        <li onClick={() => navigate('/profile')}>
+          <FontAwesomeIcon className="user-icon" icon="fa-regular fa-user"/>
+        </li>
+        <li>
+        {!memberId ? (
+            <LoginModal /> / <p onClick={() => navigate('/signup')}>SIGNUP</p>
+        ) : (
+            <LogoutModal />
+          )}
+        </li>
+      </ul>
+    </nav>
   );
 };
 
