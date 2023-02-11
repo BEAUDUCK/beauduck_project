e16f2c62ff9f5b8bfae25a12c7b8ea04a778d445
Hyokiz <janghh97@gmail.com>
2023-02-11 19:52:10 +0900

    [Feat]: feat openvidu

---
+++ beauduck-front/src/openvidu/components/stream/OvVideo copy.js
@@ -0,0 +1,29 @@
+import React, { Component, useState, useEffect, useRef } from 'react';
+import './StreamComponent.css';
+
+const OvVideoComponent = (props) => {
+	const [streamManager, setStreamManager] = useState(props.streamManager);
+	const videoRef = useRef()
+	console.log(props.user?.streamManager)
+
+	useEffect(() => {
+		if (props.streamManager) {
+			setStreamManager(props?.streamManager.addVideoElement(videoRef.current))
+		}
+	}, [props.streamManager])
+
+	return (
+		<div className='OvVideo' style={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly", alignItems: "center" }}>
+			<video
+				className='input_video'
+				autoPlay={true}
+				id={'video-' + props.user?.getStreamManager().stream.streamId}
+				ref={videoRef}
+				muted={props.mutedSound}
+				style={{ width: "30vh",height: "18vh" }}
+			/>
+	</div>
+	)
+}
+
+export default OvVideoComponent;
