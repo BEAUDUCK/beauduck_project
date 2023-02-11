dcc3fb386f262cd6c7716497186075ebeb90063e
sdflksjf2683 <gbjhgy126@gmail.com>
2023-02-12 00:37:00 +0900

    [build]: Modify pem file

--- back/flask/app.py
+++ back/flask/app.py
@@ -186,4 +186,4 @@ def ajax():
 if __name__ == '__main__':
     # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
     # ssl_context.load_cert_chain(certfile='cert.pem', keyfile='privkey.pem')
-    app.run(host='0.0.0.0', port=5000, ssl_context =('/etc/letsencrypt/live/i8b306.p.ssafy.io/cert.pem', '/etc/letsencrypt/live/i8b306.p.ssafy.io/privkey.pem'))
+    app.run(host='0.0.0.0', port=5000, ssl_context =("cert.pem", "privkey.pem"))
