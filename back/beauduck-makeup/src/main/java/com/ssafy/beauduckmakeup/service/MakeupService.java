6a26b5cc5a8bfd10c81362e5014cdf113eb4dfff
최규림 <adcdm87@gmail.com>
2023-02-11 15:32:34 +0900

    [feat]: Update makeup insert logic

--- back/beauduck-makeup/src/main/java/com/ssafy/beauduckmakeup/service/MakeupService.java
+++ back/beauduck-makeup/src/main/java/com/ssafy/beauduckmakeup/service/MakeupService.java
@@ -61,10 +61,10 @@ public class MakeupService {
         System.out.println("일단 멤버 찾기: "+memberEntity.get().getId());
         Optional<ImgaiEntity> imgaiEntity =imgaiRepository.findByMemberEntity(memberEntity.get());
 //        System.out.println("imgai에서 찾았졍: "+ imgaiEntity.get().getMemberEntity().getId());
-        ImgaiEntity imgai = imgaiEntity.get();
+//        ImgaiEntity imgai = imgaiEntity.get();
 //        System.out.println("업데이트 전: "+imgai.getIsMakeup());
-        if(imgai != null)
-            imgai.updateIsMakeup();
+        if(imgaiEntity.isPresent())
+            imgaiEntity.get().updateIsMakeup();
 //        System.out.println("업데이트 후: "+imgai.getIsMakeup());
         return makeup.getId();
     }
