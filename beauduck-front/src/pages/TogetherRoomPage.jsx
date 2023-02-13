7ad7e31aa38d403507093545cbe581f6ef036e94
Hyokiz <janghh97@gmail.com>
2023-02-14 03:24:39 +0900

    [Feat]: feat together page

--- beauduck-front/src/pages/TogetherRoomPage.jsx
+++ beauduck-front/src/pages/TogetherRoomPage.jsx
@@ -1,24 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TogetherRoomComponent from "../openvidu/components/TogetherRoomComponent";
 import getTogetherDetail from "../features/together/TogetherSlice"
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const TogetherRoomPage = () => {
+const TogetherRoomPage = ({ roomId, host }) => {
+  // useEffect(() => {
+  //   if (myNickname === state.hostNickname) {
+  //     console.log("같음")
+  //     setIsHost(true)
+  //   } else {
+  //     console.log("다름")
+  //     setIsHost(false)
+  //   }
+  //   console.log(isHost)
+  // }, [])
   const dispatch = useDispatch()
   const location = useLocation()
-
-  const { state } = location
   const myNickname = useSelector(state => state.member.nickName)
+  const { state } = location
+  const [isHost, setIsHost] = useState(myNickname === state.hostNickname)
+  const navigate = useNavigate()
 
-  console.log(state, myNickname)
 
+  console.log(isHost)
   return (
-    <div style={{ position: "fixed", zIndex: "999" ,width: "100vw", height: "100vh", backgroundColor: "white" }}>
+    <div style={{ position: "fixed", zIndex: "999" ,width: "100vw", height: "100vh", backgroundColor: "#FFF8BC" }}>
       <TogetherRoomComponent 
         sessionName={state.roomId}
         hostNickname={state.hostNickname}
         user={myNickname}
+        isHost={isHost}
+        navigate={navigate}
       />
     </div>
   );
