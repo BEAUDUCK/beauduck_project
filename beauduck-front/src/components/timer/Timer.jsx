017b446a5ffd999493f978b10799eae6d3fcd1a5
Yonghyunc <chyh9218@gmail.com>
2023-02-13 17:28:11 +0900

    [Feat]: Add Consulting Ui and idx logic

--- beauduck-front/src/components/timer/Timer.jsx
+++ beauduck-front/src/components/timer/Timer.jsx
@@ -1,22 +1,23 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 
-const Timer = () => {
+const Timer = ({ changeIdx }) => {
   const [second, setSecond] = useState(4);
   useEffect(() => {
     const countdown = setInterval(() => {
       if (parseInt(second) > 0) {
         setSecond(parseInt(second) - 1);
       } else if (parseInt(second) === 0) {
+        changeIdx();
         clearInterval(countdown);
-        setSecond(4) 
+        setSecond(4);
       }
     }, 1000);
     return () => clearInterval(countdown);
   }, [second]);
 
   return (
-    <div>
+    <div className="timer">
       <h1>{second}</h1>
     </div>
   );
