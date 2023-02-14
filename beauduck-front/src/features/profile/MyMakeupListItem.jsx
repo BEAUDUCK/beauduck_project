fbf3172d37f7ea2f006b8dc5f730e73a416708d3
Yonghyunc <chyh9218@gmail.com>
2023-02-14 11:42:01 +0900

    [Fix]: change face capture URI

--- beauduck-front/src/features/profile/MyMakeupListItem.jsx
+++ beauduck-front/src/features/profile/MyMakeupListItem.jsx
@@ -1,10 +1,22 @@
 const MyMakeupListItem = ({ makeup }) => {
+  const [isInfo, setIsInfo] = useState(false);
+
+  const isToggleInfo = () => {
+    setIsInfo(!isInfo);
+  };
+
   return (
-    <div className="makeup-item">
-      <img src={makeup.img} alt="" />
-      <p>{makeup.title}</p>
-      <button>START</button>
-    </div>
+    <>
+      <div className="makeup-item">
+        <img src={makeup.img} alt="" />
+        <p>{makeup.title}</p>
+        <button onClick={isToggleInfo}>START</button>
+      </div>
+      {isInfo && (
+        <SingleModalInfo makeupId={modeItem.id} isToggleInfo={isToggleInfo} />
+      )}
+      {isInfo && <BlackOut onClickEvent={isToggleInfo} />}
+    </>
   );
 };
 
