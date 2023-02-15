5b073127c5ae4b9a428bb4d4d6e382cdf99c5184
김아영 (KIM A YOUNG) <joen00@naver.com>
2023-02-15 10:09:04 +0900

    [fix] : requirement.txt down

--- back/flask/app.py
+++ back/flask/app.py
@@ -39,25 +39,14 @@ def getEmps(memberId): #imgai 테이블에서 메이크업이 있는 멤버만 �
 # v2 => 이미지  불러오고 변환하기
 def get_cropped_face(image_file):
     image_file = image_file[23:] #data:image/jpeg;base64, 잘라내기
-    
     imgdata  = base64.b64decode(image_file)
     dataBytesIO  = io.BytesIO(imgdata)
     image = Image.open(dataBytesIO)
-    # image_nparray = np.asarray(bytearray(image), dtype=np.unit8)
     image_nparray = np.array(image)
-    # print("nparray: ",image_nparray)
-    
-    # image = cv2.imdecode(image_nparray, cv2.IMREAD_COLOR) 
-    face_locations = face_recognition.face_locations(image_nparray)   # 얼굴 영역 박스 
-    
-    # print(face_locations) 
-    
+    face_locations = face_recognition.face_locations(image_nparray)   # 얼굴 영역 박스   
     a, b, c, d = face_locations[0]     # 얼굴 영역 박스 좌표
     cropped_face = image_nparray[a:c,d:b,:]    # 얼굴 영역 박스 좌표를 이용해 얼굴 잘라내기 
-
-    # cropped_face.show()
     return cropped_face # 이미지 파일
-
 
 
 def get_face_embedding(face):
@@ -184,7 +173,6 @@ def save():
 
     memberId = request.get_json()["memberId"]
     url = request.get_json()["img"]
-
 
     #d얼굴 사진으로 임베딩 벡터 추출하기
     embedding = get_face_embedding_dict(memberId,url)
