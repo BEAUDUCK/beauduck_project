f52e2001d1bd01b906a387b4a64497f17b59a7e5
choi1087 <adcdm87@gmail.com>
2023-02-15 15:07:18 +0900

    [feat]: Cancel Redis config

--- back/beauduck-consult/src/main/java/com/ssafy/beauduckconsult/config/RedisConfiguration.java
+++ back/beauduck-consult/src/main/java/com/ssafy/beauduckconsult/config/RedisConfiguration.java
@@ -11,23 +11,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfiguration {
 
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration()
-                .master("mymaster")
-                .sentinel("127.0.0.1", 26379)
-                .sentinel("127.0.0.1", 26380)
-                .sentinel("127.0.0.1", 26381);
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisSentinelConfiguration);
-        return lettuceConnectionFactory;
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory){
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);;
-        redisTemplate.setKeySerializer(new StringRedisSerializer());;
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        return redisTemplate;
-    }
+//    @Bean
+//    public LettuceConnectionFactory redisConnectionFactory() {
+//        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration()
+//                .master("mymaster")
+//                .sentinel("127.0.0.1", 26379)
+//                .sentinel("127.0.0.1", 26380)
+//                .sentinel("127.0.0.1", 26381);
+//        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisSentinelConfiguration);
+//        return lettuceConnectionFactory;
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory){
+//        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory);;
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());;
+//        redisTemplate.setValueSerializer(new StringRedisSerializer());
+//        return redisTemplate;
+//    }
 }
