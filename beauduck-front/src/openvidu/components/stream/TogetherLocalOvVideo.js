dfe94a71f77ffc36236829c30c57a017648102f9
Hyokiz <janghh97@gmail.com>
2023-02-15 11:03:12 +0900

    [Fix]: fixed error code

--- beauduck-front/src/openvidu/components/stream/TogetherLocalOvVideo.js
+++ beauduck-front/src/openvidu/components/stream/TogetherLocalOvVideo.js
@@ -40,7 +40,7 @@ export default class TogetherLocalOvVideoComponent extends Component {
 							id={'video-' + this.props.user.getStreamManager().stream.streamId}
 							ref={this.videoRef}
 							muted={this.props.mutedSound}
-							style={{ width: "100%", height: "100%", borderRadius: "25px" }}
+							style={{ width: "100%", height: "100%", borderRadius: "25px"}}
 					/>
 				</div>
 			);
