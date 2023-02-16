ac793605b042d53d413597c83fbc310756be66bb
박주승 <juseungpark@naver.com>
2023-02-16 09:53:58 +0900

    [fix]:header and router

--- beauduck-front/src/pages/MainPage.jsx
+++ beauduck-front/src/pages/MainPage.jsx
@@ -11,7 +11,7 @@ const MainPage = () => {
         <section id="section1" >
         </section>
         <div id='main-line' className="demo">
-        <a href="#section2">+
+        <a href="#section2">
             <span></span>scroll</a>
         </div>
         <section id='section2'>
@@ -23,7 +23,7 @@ const MainPage = () => {
               <h1>도와덕</h1>
               <p>다수의 사용자에게 퍼스널 컬러 진단을 무료로 받을 수 있는 서비스입니다. </p>
               {/* 도와덕 링크로 바꾸기 */}
-              <Link to="/no">자세히 보기</Link>
+              <Link to="/help">자세히 보기</Link>
             </div>
           </div>
           <div className='section2-2'>
@@ -31,7 +31,7 @@ const MainPage = () => {
               <h1>따라해덕</h1>
               <p>최신 AI 기술로 뷰덕 유저들의 다양한 화장법을 따라해 볼 수 있는 서비스입니다.</p>
               {/* 따라해덕 링크로 바꾸기 */}
-              <Link to="/no">자세히 보기</Link>
+              <Link to="/single">자세히 보기</Link>
             </div>
             <div>
               <img className="img-box2-2" src={require("../assets/test.gif")}/>
@@ -45,7 +45,7 @@ const MainPage = () => {
               <h1>투게덕</h1>
               <p>화상 채팅을 통해 뷰덕들이 모여 함께 화장 할 수 있는 서비스입니다.</p>
               {/* 투게덕 링크로 바꾸기 */}
-              <Link to="/no">자세히 보기</Link>
+              <Link to="/together">자세히 보기</Link>
             </div>
           </div>
         </section>
