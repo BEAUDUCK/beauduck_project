e21720b6f215c0801dae3dd54e9d2be064a5369a
Yonghyunc <chyh9218@gmail.com>
2023-02-17 06:27:08 +0900

    [Fix]: change routing

--- beauduck-front/src/openvidu/components/AllComp.jsx
+++ beauduck-front/src/openvidu/components/AllComp.jsx
@@ -49,7 +49,7 @@ const AllComp = ({
 
   // 진단 종료
   useEffect(() => {
-    if (nowIdx === 30) {
+    if (nowIdx === 10) {
       console.log('인덱스 종료');
       dispatch(setExerciseStatus('done'));
     }
