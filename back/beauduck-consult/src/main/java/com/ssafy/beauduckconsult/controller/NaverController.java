fc3063fec015b9d63d00b31476c71e69b48a1eb7
choi1087 <adcdm87@gmail.com>
2023-02-16 15:33:22 +0900

    [refactor]: Update Naver Controller

--- back/beauduck-consult/src/main/java/com/ssafy/beauduckconsult/controller/NaverController.java
+++ back/beauduck-consult/src/main/java/com/ssafy/beauduckconsult/controller/NaverController.java
@@ -32,18 +32,13 @@ public class NaverController {
             throw new RuntimeException("검색어 인코딩 실패",e);
         }
 
-
         String apiURL = "https://openapi.naver.com/v1/search/blog?query=" + text;    // JSON 결과
         //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // XML 결과
-
 
         Map<String, String> requestHeaders = new HashMap<>();
         requestHeaders.put("X-Naver-Client-Id", clientId);
         requestHeaders.put("X-Naver-Client-Secret", clientSecret);
         String responseBody = get(apiURL,requestHeaders);
-
-
-        System.out.println(responseBody);
         return responseBody;
     }
 
@@ -59,18 +54,13 @@ public class NaverController {
             throw new RuntimeException("검색어 인코딩 실패",e);
         }
 
-
         String apiURL = "https://openapi.naver.com/v1/search/shop?query=" + text;    // JSON 결과
         //String apiURL = "https://openapi.naver.com/v1/search/blog.xml?query="+ text; // XML 결과
-
 
         Map<String, String> requestHeaders = new HashMap<>();
         requestHeaders.put("X-Naver-Client-Id", clientId);
         requestHeaders.put("X-Naver-Client-Secret", clientSecret);
         String responseBody = get(apiURL,requestHeaders);
-
-
-        System.out.println(responseBody);
         return responseBody;
     }
 
@@ -86,16 +76,12 @@ public class NaverController {
             throw new RuntimeException("검색어 인코딩 실패",e);
         }
 
-
-        System.out.println("text = " + text);
         String apiURL = "https://openapi.naver.com/v1/search/news?query=" + text;    // JSON 결과
-
 
         Map<String, String> requestHeaders = new HashMap<>();
         requestHeaders.put("X-Naver-Client-Id", clientId);
         requestHeaders.put("X-Naver-Client-Secret", clientSecret);
         String responseBody = get(apiURL,requestHeaders);
-
         return responseBody;
     }
     private static String get(String apiUrl, Map<String, String> requestHeaders) {
